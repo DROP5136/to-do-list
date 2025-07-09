7be8a495b8fe0b5c32de2be51a24d2c97eb9516e
DROP5136 <batralakshay16@gmail.com>
2025-07-10 01:00:39 +0530

    version2

--- todo.js
+++ todo.js
@@ -3,10 +3,16 @@ function haha(){
     console.log(todo.value)
     let arr=JSON.parse(localStorage.getItem("arr")) || []
     arr.push(todo.value)
+    let htm="";
+    for(let i=0;i<arr.length;i++){
+        htm+=arr[i]+"<br>"
+    }
+    document.querySelector(".para").innerHTML=htm
     console.log(arr)
     localStorage.setItem("arr",JSON.stringify(arr))
     todo.value="";
 }
 function clc(){
     localStorage.removeItem("arr")
+    document.querySelector(".para").innerHTML=""
 }
