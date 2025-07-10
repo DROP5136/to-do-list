ea03b545ce85c8d7122e02c7b756fd2c864d0b94
DROP5136 <batralakshay16@gmail.com>
2025-07-10 14:03:09 +0530

    fully-functional-without-css

--- todo.js
+++ todo.js
@@ -1,18 +1,43 @@
 function haha(){
     const todo=document.querySelector(".todo")
-    console.log(todo.value)
+    const datee=document.querySelector(".dte")
+    
     let arr=JSON.parse(localStorage.getItem("arr")) || []
+    let drr=JSON.parse(localStorage.getItem("drr")) || []
     arr.push(todo.value)
-    let htm="";
-    for(let i=0;i<arr.length;i++){
-        htm+=arr[i]+"<br>"
-    }
-    document.querySelector(".para").innerHTML=htm
-    console.log(arr)
+    drr.push(datee.value)
+    
+    
     localStorage.setItem("arr",JSON.stringify(arr))
-    todo.value="";
+    localStorage.setItem("drr",JSON.stringify(drr))
+    display()
+    todo.value=""
+    datee.value=""
+    
 }
 function clc(){
     localStorage.removeItem("arr")
+    localStorage.removeItem("drr")
     document.querySelector(".para").innerHTML=""
 }
+function del(i){
+    let arr=JSON.parse(localStorage.getItem("arr")) || []
+    let drr=JSON.parse(localStorage.getItem("drr")) || []
+    arr.splice(i, 1);
+    drr.splice(i, 1);
+    localStorage.setItem("arr",JSON.stringify(arr))
+    localStorage.setItem("drr",JSON.stringify(drr))
+    display()
+}
+function display(){
+    let arr = JSON.parse(localStorage.getItem("arr")) || [];
+    let drr = JSON.parse(localStorage.getItem("drr")) || [];
+
+    let htm="";
+    for(let i=0;i<arr.length;i++){
+        htm+=arr[i]+" "+drr[i]+" "+"<button onclick='del("+i+")'>Delete</button>"+"<br>"
+    }
+    document.querySelector(".para").innerHTML=htm
+    
+}
+window.onload = display
